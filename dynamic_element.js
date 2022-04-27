fe13a5a671e035b1f265a763653f603b19126a2f
durongze <durongze@qq.com>
2022-04-28 01:55:54 +0800

    Update dynamic_element.js

--- dynamic_element.js
+++ dynamic_element.js
@@ -1,61 +1,98 @@
+<script src="./b64.js"></script>
+<script src="../LZWEncoder.js"></script>
+<script src="../NeuQuant.js"></script>
+<script src="../GIFEncoder.js"></script>
+<script src="../hanzi-writer.js"></script>
+<script src="../jquery-3.4.1.js"></script>
+<script src="../html2canvas.js"></script>
+<script src="../canvg.js"></script>
+<canvas id="bitmap_red" width = "100px" height="100px" ></canvas>
+<canvas id="bitmap_green" width = "100px" height="100px" ></canvas>
+<img id="image">
+<div id="wheat_div" class="box"></div>
+<div id="mind_div" class="box"></div>
+<style type="text/css">
+.box {width: 100px; height: 100px; background: red; border: 5px solid yellow;}
+</style>
+<script>
 	var svg_tmr;
-	var wheat_idx = 0;
-	var writer_wheat = HanziWriter.create('wheat_div', '麦', {
-		width: 100,
-		height: 100,
-		padding: 5,
-		delayBetweenLoops: 3000
-	});   
-	writer_wheat.loopCharacterAnimation();  
+	var img_idx = 0;
 
-	var canvas = document.getElementById('bitmap');
-	var context = canvas.getContext('2d');
+	var wheat = HanziWriter.create('wheat_div', '麦', {width:100, height:100, padding:5, delayBetweenLoops:3000});
+	wheat.loopCharacterAnimation();  
+	var mind = HanziWriter.create('mind_div', '思', {width:100, height:100, padding:5, delayBetweenLoops:3000});
+	mind.loopCharacterAnimation();
 
-	context.fillStyle = "rgb(255, 0, 0)";
-	context.fillRect(0,0,canvas.width, canvas.height); 
+	var encoder = null;
+	function CreateGif(width, height)
+	{
+		encoder = new GIFEncoder();
+		console.log(encoder.start());
+		encoder.setRepeat(1000); //auto-loop
+		encoder.setDelay(200);
+		encoder.setSize(width, height);
 
-	var encoder = new GIFEncoder();
-	console.log(encoder.start());
-	encoder.setRepeat(0); //auto-loop
-	encoder.setDelay(500);
-	encoder.setSize(100, 100);
+		var canvas_red = document.getElementById('bitmap_red');
+		var context_red = canvas_red.getContext('2d');
+		context_red.fillStyle = "rgb(255, 0, 0)";
+		context_red.fillRect(0,0,canvas_red.width, canvas_red.height); 
 
-	function Svg2CanvasWheat() {
-		if (wheat_idx > 10) {
+		var canvas_green = document.getElementById('bitmap_green');
+		var context_green = canvas_green.getContext('2d');
+		context_green.fillStyle = "rgb(0, 255, 0)";
+		context_green.fillRect(0, 0, canvas_green.width, canvas_green.height); 
+
+		encoder.addFrame(context_red);
+		encoder.addFrame(context_green);
+	}
+
+	function UpdateNode(parentNode, canvas_svg)
+	{
+		if (parentNode.childNodes.length > 1) {
+			parentNode.replaceChild(canvas_svg, parentNode.lastChild);
+		} else {
+			parentNode.appendChild(canvas_svg);
+		}
+	}
+
+	function SaveCanvas(canvas_svg)
+	{
+		var strDataURI = canvas_svg.toDataURL("image/jpeg");
+		var image = strDataURI.replace("image/jpeg", "image/octet-stream");
+		var link = document.createElement('a');
+		link.setAttribute('href', image);
+		link.setAttribute('download', img_idx + '.jpg');
+		// link.click();
+		img_idx += 1;
+	}
+
+	function SvgCallBack(index, node) {
+		var parentNode = ;
+		var svg = node.outerHTML.trim();
+
+		var canvas_svg = document.createElement('canvas');
+		canvas_svg.id = 'svg_canvas';
+		canvg(canvas_svg, svg);
+
+		UpdateNode(node.parentNode, canvas_svg);
+
+		encoder.addFrame(canvas_svg.getContext('2d'));
+
+		SaveCanvas(canvas_svg);
+	}
+
+	function Svg2Canvas() {
+		if (img_idx > 26) {
 			clearInterval(svg_tmr);
 			encoder.finish();
 			document.getElementById('image').src = 'data:image/gif;base64,'+encode64(encoder.stream().getData());
 			return ;
 		}
-		var svgElem = $("#wheat_div").find('svg');
-		svgElem.each(function (index, node) {
-			var parentNode = node.parentNode;
-			var svg = node.outerHTML.trim();
-
-			var canvas_svg = document.createElement('canvas');
-			canvg(canvas_svg, svg);
-			console.log('parentNode.id:' + parentNode.id + ":" + parentNode.childNodes.length);
-			canvas_svg.id = 'svg_canvas';
-			if (parentNode.childNodes.length > 1) {
-				parentNode.replaceChild(canvas_svg, parentNode.lastChild);
-			} else {
-				parentNode.appendChild(canvas_svg);
-			}
-			var strDataURI = canvas_svg.toDataURL("image/jpeg");
-			var image = strDataURI.replace("image/jpeg", "image/octet-stream");
-			var link = document.createElement('a');
-			link.setAttribute('href', image);
-			link.setAttribute('download', 'wheat_div' + wheat_idx + '.jpg');
-			// link.click();
-			wheat_idx += 1;
-		});
-		// var imageData = $("#wheat_div").lastChild.toDataURL("image/jpeg");
-		// encoder.addFrame(imageData, true);
-		var svg_canvas = $("#wheat_div").find('canvas');
-		// var svg_context = svg_canvas.getContext('2d');
-		console.log('svg_canvas:' + svg_canvas[0]);
-		encoder.addFrame(context);
-		// encoder.addFrame(document.getElementById('svg_context').getContext('2d'));
+		var img_div = img_idx < 13 ? "#wheat_div" : "#mind_div";
+		var svgElem = $(img_div).find('svg');
+		svgElem.each(SvgCallBack);
 	};
 
-	svg_tmr = setInterval("Svg2CanvasWheat()", 1000);
+	svg_tmr = setInterval("Svg2Canvas()", 1000);
+
+</script>
